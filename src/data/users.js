dcd7f37a1e0e131a395936f14734cce8508b0c65
danielmarques28 <danielmarques7@hotmail.com>
2021-02-02 12:15:40 -0300

    Add fake data

---
+++ src/data/users.js
@@ -0,0 +1,74 @@
+export default function usersList() {
+  return [
+    {
+      id: 1,
+      avatar: 'https://menshairstyleswag.com/wp-content/uploads/2018/10/kendrick-lamar-new-haircut-with-braids.jpg',
+      nickname: 'Kung Fu Kenny',
+      name: 'Kendrick Lamar',
+      birth: '1990-02-27',
+      gender: 'Male',
+      office: 'Rapper',
+      phone: '+1 909-510-5912',
+      address: 'California, USA',
+      telegram: 'kdot',
+      instagram: 'kdot_',
+      twitter: 'kdot12'
+    },
+    {
+      id: 2,
+      avatar: 'https://s.abcnews.com/images/US/kanye-west-mo_hpMain_20200704-214451_1x1_992.jpg',
+      nickname: 'Yeezy',
+      name: 'Kanye West',
+      birth: '1989-04-21',
+      gender: 'Male',
+      office: 'Rapper',
+      phone: '+1 456-231-3425',
+      address: 'California, USA',
+      telegram: 'yeezy',
+      instagram: 'yeezy_',
+      twitter: 'yeezy12'
+    },
+    {
+      id: 3,
+      avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-Gz86-V0ZuM4ilujifoUEKQBrv8qvxzP9OA&usqp=CAU',
+      nickname: 'SZA',
+      name: 'SZA',
+      birth: '1991-01-14',
+      gender: 'Female',
+      office: 'Singer',
+      phone: '+1 544-987-5643',
+      address: 'California, USA',
+      telegram: 'sza',
+      instagram: 'sza_',
+      twitter: 'sza12'
+    },
+    {
+      id: 4,
+      avatar: 'https://townsquare.media/site/812/files/2019/09/schoolboy-q.jpg?w=980&q=75',
+      nickname: 'Groovy Tony',
+      name: 'Schoolboy Q',
+      birth: '1992-03-31',
+      gender: 'Male',
+      office: 'Rapper',
+      phone: '+1 331-324-4668',
+      address: 'California, USA',
+      telegram: 'groovy',
+      instagram: 'groovy_',
+      twitter: 'groovy12'
+    },
+    {
+      id: 5,
+      avatar: 'https://catracalivre.com.br/wp-content/thumbnails/0Oh0VShfA69kovvzASECqqkGtRM=/wp-content/uploads/2017/02/Frank_Ocean.png',
+      nickname: 'Frank Ocean',
+      name: 'Frank Ocean',
+      birth: '1993-10-15',
+      gender: 'Male',
+      office: 'Singer',
+      phone: '+1 184-511-1255',
+      address: 'California, USA',
+      telegram: 'frank',
+      instagram: 'frank_',
+      twitter: 'frank12'
+    }
+  ];
+};
